888be1af71863c67b62c5fedbf65bb318ed8687e
cc189 <cc189dev@gmail.com>
2020-03-21 21:51:53 -0700

    add initData

--- example/ExampleContainer.tsx
+++ example/ExampleContainer.tsx
@@ -2,92 +2,89 @@ import * as React from 'react';
 import { Draggable as BaseDraggable, Droppable as BaseDroppable, DropLine } from "../src/index";
 import { DroppableProps } from "../src/Droppable/interface";
 import { DraggableProps } from "../src/Draggable/interface";
+import { initData } from "./initData";
 
-export default class ExampleContainer extends React.Component {
-    handleOnDragEnd(data: any) {
-        console.log('>>> handleOnDragEnd.data', data);
+export default class ExampleContainer extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            initChildren: initData
+        };
     }
+    handleOnDragEnd(data: any) {
+        const {source, destination, draggableId, type} = data;
+        console.log('>>> handleOnDragEnd', new Date().getTime(), data);
+        this.setState({initChildren: [...initData]})
+    }
+
     render() {
+        const {initChildren} = this.state;
         const Droppable = (props: DroppableProps) => {
-            return <BaseDroppable {...props} onDragEnd={this.handleOnDragEnd}>{props.children}</BaseDroppable>
+            return <BaseDroppable {...props} onDragEnd={(data: any) => this.handleOnDragEnd(data)}>{props.children}</BaseDroppable>
         }
         const Draggable = (props: DraggableProps) => {
-            return <BaseDraggable {...props} onDragEnd={this.handleOnDragEnd}>{props.children}</BaseDraggable>
+            return <BaseDraggable {...props} onDragEnd={(data: any) => this.handleOnDragEnd(data)}>{props.children}</BaseDraggable>
         }
+        const renderChildren = (children: any[]) => {
+            return children.map((child, index) => {
+                const { type, children, id } = child;
+                if (type === "Grid") {
+                    return (
+                        <Draggable  key={id} draggableId={`${id}-drag`} type={type} index={index}>
+                            {({ handler }) => (
+                                <div {...handler}>
+                                    <Droppable key={id} droppableId={`${id}-drop`} type={type}>
+                                        {({ handler, status }) => (
+                                            <div {...handler}
+                                                style={{
+                                                    backgroundColor: status.isDraggingOver ? "lightgrey" : "#ed93fd",
+                                                    margin: 20,
+                                                    padding: 20,
+                                                    minHeight: 60,
+                                                    border: `1px solid grey`,
+                                                    display: "flex",
+                                                    flexDirection: "column"
+                                                }}
+                                            >
+                                                {renderChildren(children)}
+                                            </div>
+                                        )}
+                                    </Droppable>
+                                </div>
+                            )}
+                        </Draggable>
+                    )
+                } else {
+                    return (
+                        <Draggable draggableId={id} key={id} index={index} type={type}>
+                            {({ handler }) => (
+                                <span {...handler} style={{ padding: 10, backgroundColor: "white" }}>
+                                    {handler.id}
+                                </span>
+                            )}
+                        </Draggable>
+                    )
+                }
+            })
+        }
+
         return (
             <>
-            <DropLine/>
-            <Droppable droppableId={"drop01"}>
-                {({ handler }) => (
-                    <div {...handler} 
-                    style={{ 
-                        height: `100vh`,
-                        backgroundColor: handler.idDraggingOver ? "#9436a5" : "lightyellow",
-                        display: 'flex',
-                        flexDirection: 'column'
-                        }}>
-                        {handler.id}
-
-                        {["drag01", "drag02", "drag03", "drag04", "drag05", "drag06"].map((draggableId: string, index: number) => {
-                            return (
-                                <Draggable draggableId={draggableId} key={draggableId} index={index}>
-                                    {({ handler }) => (
-                                        <span {...handler} style={{ padding: 10, backgroundColor: "white" }}>
-                                            {handler.id}
-                                        </span>
-                                    )}
-                                </Draggable>
-                            )
-                        })}
-
-                        <Draggable draggableId={"drag06"}>
-                            {({ handler }) => (
-                                <div {...handler}>
-                                    <Droppable droppableId={"drop02"}>
-                                        {({ handler }) => (
-                                            <div {...handler}
-                                                style={{
-                                                    backgroundColor: handler.idDraggingOver ? "#9436a5" : "#ed93fd",
-                                                    margin: 10,
-                                                    minHeight: 60,
-                                                    border: `1px solid grey`,
-                                                    display: "flex",
-                                                    flexDirection: "column"
-                                                }}
-                                            >
-                                                {handler.id}
-                                            </div>
-                                        )}
-                                    </Droppable>
-                                </div>
-                            )}
-                        </Draggable>
-
-                        <Draggable draggableId={"drag07"}>
-                            {({ handler }) => (
-                                <div {...handler}>
-                                    <Droppable droppableId={"drop03"}>
-                                        {({ handler }) => (
-                                            <div {...handler}
-                                                style={{
-                                                    backgroundColor: handler.idDraggingOver ? "#9436a5" : "#ed93fd",
-                                                    margin: 10,
-                                                    minHeight: 60,
-                                                    border: `1px solid grey`,
-                                                    display: "flex",
-                                                    flexDirection: "column"
-                                                }}
-                                            >
-                                                {handler.id}
-                                            </div>
-                                        )}
-                                    </Droppable>
-                                </div>
-                            )}
-                        </Draggable>
-                    </div>
-                )}
-            </Droppable>
+                <Droppable droppableId={"mainContainer"} type={"CONTAINER"} index={0}>
+                    {({ handler }) => (
+                        <div {...handler}
+                            style={{
+                                height: `100vh`,
+                                backgroundColor: handler.isDraggingOver ? "#9436a5" : "lightyellow",
+                                display: 'flex',
+                                flexDirection: 'column'
+                            }}
+                        >
+                            {renderChildren(initChildren)}
+                            <DropLine/>
+                        </div>
+                    )}
+                </Droppable>
             </>
         )
     }
---
+++ example/initData.ts
@@ -0,0 +1,33 @@
+export const initData = [{
+    "id": "Grid-1",
+    "type": "Grid",
+    "children": [
+        {
+            "id": "Grid-2",
+            "type": "Grid",
+            "children": [
+                {
+                    "id": "Button-1",
+                    "type": "Button",
+                    "children": [],
+                }
+            ]
+        },
+        {
+            "id": "Grid-3",
+            "type": "Grid",
+            "children": [
+                {
+                    "id": "Button-2",
+                    "type": "Button",
+                    "children": [],
+                },
+                {
+                    "id": "Button-3",
+                    "type": "Button",
+                    "children": []
+                }
+            ]
+        }
+    ]
+}]
